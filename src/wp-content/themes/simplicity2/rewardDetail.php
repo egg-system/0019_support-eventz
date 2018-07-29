c4e63c36b8753eceede44d404b068ee061033495
hikarut <ht.hikaru.takahashi@gmail.com>
2018-07-29 16:12:41 +0900

    bootstrap追加

--- src/wp-content/themes/simplicity2/rewardDetail.php
+++ src/wp-content/themes/simplicity2/rewardDetail.php
@@ -11,14 +11,33 @@ error_log(print_r($results,true)."\n", 3, "/tmp/error.log");
 
 ?>
 
-<?php if (isset($results[0])) { ?>
-    <div>
-    id:<?php echo $results[0]['id']; ?><br>
-    紹介者ID:<?php echo $results[0]['introducer_id']; ?><br>
-    日付:<?php echo $results[0]['date']; ?><br>
-    会員レベル:<?php echo $results[0]['level']; ?><br>
-    金額:<?php echo $results[0]['price']; ?><br>
-    </div>
+<!--TODO:bootstrapの読み込み方とタイミングを変える-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<?php if (isset($results)) { ?>
+    <table class="table">
+        <thead>
+            <tr>
+                <th>id</th>
+                <th>紹介者ID</th>
+                <th>日付</th>
+                <th>会員レベル</th>
+                <th>金額</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($results as $result) { ?>
+                <tr>
+                    <th><?php echo $result['id']; ?></th>
+                    <td><?php echo $result['introducer_id']; ?></td>
+                    <td><?php echo $result['date']; ?></td>
+                    <td><?php echo $result['level']; ?></td>
+                    <td><?php echo $result['price']; ?></td>
+                </tr>
+        <?php } ?>
+        </tbody>
+    </table>
 <?php } else { ?>
     <div>報酬はありません</div>
 <?php } ?>
+
