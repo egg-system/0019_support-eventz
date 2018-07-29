d050923cbeb95e6cd83f372b367403a0ce7dd5ec
hikarut <ht.hikaru.takahashi@gmail.com>
2018-07-29 15:33:02 +0900

    修正

--- src/wp-content/themes/simplicity2/rewardDetail.php
+++ src/wp-content/themes/simplicity2/rewardDetail.php
@@ -6,16 +6,19 @@ $id = SwpmMemberUtils::get_logged_in_members_id();
 // 報酬詳細テーブルからデータを取得
 $tableName = $table_prefix . "reward_details";
 $sql = $wpdb->prepare("SELECT * FROM ${tableName} WHERE member_id = %d", $id);
-error_log($sql."\n", 3, "/tmp/error.log");
 $results = $wpdb->get_results($sql, ARRAY_A);
 error_log(print_r($results,true)."\n", 3, "/tmp/error.log");
 
 ?>
 
-<div>
-id:<?php echo $results[0]['id']; ?><br>
-紹介者ID:<?php echo $results[0]['introducer_id']; ?><br>
-日付:<?php echo $results[0]['date']; ?><br>
-会員レベル:<?php echo $results[0]['level']; ?><br>
-金額:<?php echo $results[0]['price']; ?><br>
-</div>
+<?php if (isset($results[0])) { ?>
+    <div>
+    id:<?php echo $results[0]['id']; ?><br>
+    紹介者ID:<?php echo $results[0]['introducer_id']; ?><br>
+    日付:<?php echo $results[0]['date']; ?><br>
+    会員レベル:<?php echo $results[0]['level']; ?><br>
+    金額:<?php echo $results[0]['price']; ?><br>
+    </div>
+<?php } else { ?>
+    <div>報酬はありません</div>
+<?php } ?>
