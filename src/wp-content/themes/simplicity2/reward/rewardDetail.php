cfc86e2eb48f5a8d94dc9632f01161d16548b0be
hikarut <ht.hikaru.takahashi@gmail.com>
2018-08-07 08:55:00 +0900

    リファクタリング(クラス化)

--- src/wp-content/themes/simplicity2/reward/rewardDetail.php
+++ src/wp-content/themes/simplicity2/reward/rewardDetail.php
@@ -1,4 +1,167 @@
 <?php
+
+class RewardDetail
+{
+    // 最大期間
+    const MAX_TERM = 6;
+
+    // テンプレートで使う変数
+    public $start;
+    public $end;
+    public $results;
+    public $allMonth;
+    public $inputData;
+
+    /**
+     * メイン処理(テンプレートに必要なデータのセット)
+     *
+     * @return void
+     */
+    public function setTemplateData()
+    {
+        $this->setParam();
+    }
+
+    /**
+     * 引数の取得
+     *
+     * @return void
+     */
+    private function setParam()
+    {
+        // パラメータの取得
+        $this->start = $_GET['start'];
+        $this->end = $_GET['end'];
+    }
+
+    /**
+     * 引数のチェック
+     *
+     * @param int $start
+     * @param int $end
+     * @return boolean
+     */
+    private function checkParam($start, $end)
+    {
+        if (strlen($start) !== 6 && strlen($end) !== 6) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * 期間の取得
+     *
+     * @return void
+     */
+    private function getMonth()
+    {
+        // 取得する期間
+        $maxMonth = date("Ym");
+        $term = self::MAX_TERM;
+        $minMonth = date("Ym",strtotime("-${term} month"));
+        // ループして期間の全ての月を出す
+        $allMonth = [];
+        for ($i = 0; $i < $term; $i++) {
+            $allMonth[] = date("Ym",strtotime("-${i} month"));
+        }
+        // 月を昇順でソート
+        asort($allMonth);
+
+        return $allMonth;
+    }
+
+    /**
+     * DBから報酬データの取得
+     *
+     * @param int $start
+     * @param int $end
+     * @return array $results
+     */
+    private function getRewardData($start, $end)
+    {
+        // 必要なテーブルの定義
+        $rewardDetailsTable = $table_prefix . "reward_details";
+        $membersTable = $table_prefix . "swpm_members_tbl";
+        $memberShipTable = $table_prefix . "swpm_membership_tbl";
+
+        $bindSql = <<<SQL
+SELECT rd.id,
+       DATE_FORMAT(rd.date, '%Y%m') as date,
+       rd.price,
+       me.member_id,
+       me.first_name,
+       ms.alias
+FROM ${rewardDetailsTable} rd
+LEFT JOIN ${membersTable} me
+    ON rd.introducer_id = me.member_id 
+LEFT JOIN ${memberShipTable} ms
+    ON rd.level = ms.id 
+WHERE rd.member_id = %d
+AND DATE_FORMAT(rd.date, '%Y%m') >= ${start}
+AND DATE_FORMAT(rd.date, '%Y%m') <= ${end}
+ORDER BY rd.date
+SQL;
+        $sql = $wpdb->prepare($bindSql, $id);
+        $results = $wpdb->get_results($sql, ARRAY_A);
+
+        return $results;
+    }
+
+    /**
+     * 入金出金データをセット
+     *
+     * @param array $results
+     * @return void
+     */
+    private function setInputOutput($results)
+    {
+        $inputData = [];
+        $outputData = [];
+
+        // 早期リターン
+        if (empty($results)) {
+            $this->inputData = $inputData;
+            $this->outputData = $outputData;
+            return true;
+        }
+
+        foreach ($results as $record) {
+            // 入金データ
+            if ($record['price'] > 0) {
+                // 紹介者IDと月ごとでまとめる
+                $inputData[$record['member_id']][$record['date']] = $record;
+
+                // 1番最初のデータが1番はじめに登録されたデータなのでそれを代表のデータとしてまとめる
+                if (!isset($inputData[$record['member_id']][0])) {
+                    $inputData[$record['member_id']][0] = $record;
+                }
+            // 出金データ
+            } else {
+                // 月ごとでまとめる
+                if (!isset($outputData[$record['date']])) {
+                    $outputData[$record['date']] = $record['price'];
+                } else {
+                    $outputData[$record['date']] += $record['price'];
+                }
+            }
+        }
+        $this->inputData = $inputData;
+        $this->outputData = $outputData;
+    }
+}
+$rewardDetail = new RewardDetail();
+$rewardDetail->setTemplateData();
+error_log($rewardDetail->start."\n", 3, "/tmp/hikaru_error.log");
+error_log($rewardDetail->end."\n", 3, "/tmp/hikaru_error.log");
+
+// パラメータの取得
+$start = $_GET['start'];
+$end = $_GET['end'];
+//$check = checkParam($start, $end);
+//error_log($check."\n", 3, "/tmp/error.log");
+//error_log(gettype($check)."\n", 3, "/tmp/error.log");
  
 // メンバーIDの取得
 $id = SwpmMemberUtils::get_logged_in_members_id();
@@ -66,7 +229,6 @@ if (!empty($results)) {
         }
     }
 }
-error_log(print_r($outputData,true)."\n", 3, "/tmp/error.log");
 ?>
 
 <!--TODO:bootstrapの読み込み方とタイミングを変える-->
@@ -76,6 +238,20 @@ error_log(print_r($outputData,true)."\n", 3, "/tmp/error.log");
 // 画面いっぱいにする
 document.getElementById('main').style.width = '100%';
 </script>
+<div>表示期間：</div>
+
+<form class="form-inline" action="reward_detail_test" method="get">
+  <div class="form-group">
+    <label>開始</label>
+    <input type="number" class="form-control" placeholder="201801" name="start" value="11">
+  </div>
+  <div class="form-group">
+    <label>終了</label>
+    <input type="number" class="form-control" placeholder="201806" name="end" value="22">
+  </div>
+  <button type="submit" class="btn btn-primary">変更</button>
+</form>
+
 <?php if (!empty($results)) { ?>
 <div class="table-responsive">
     <table class="table table-condensed">
