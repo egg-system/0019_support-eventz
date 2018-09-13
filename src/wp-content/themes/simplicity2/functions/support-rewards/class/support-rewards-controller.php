859a922ae2f7ecfde3cc6f1839fbdeae5d12e390
nuno929 <t-mark5@kce.biglobe.ne.jp>
2018-09-13 20:59:23 +0900

    csv出力の追加

--- src/wp-content/themes/simplicity2/functions/support-rewards/class/support-rewards-controller.php
+++ src/wp-content/themes/simplicity2/functions/support-rewards/class/support-rewards-controller.php
@@ -3,11 +3,13 @@
 require('support-reward-table.php');
 
 class SupportRewardsController {
+    private $csvFilePath = null; 
     private $rewardTable = null;
 
     public function __construct()
     {
         $this->rewardTable = new supportRewardTable();
+        $this->csvFilePath = dirname(__DIR__) . '/csv';
     }
     
     public function index()
@@ -16,6 +18,21 @@ class SupportRewardsController {
 
         // todo：ぺジネーション処理を追加
         $rewardTable->prepare_items();
+		
         include(dirname(__DIR__) . '/view/reward.php'); 
+    }
+	
+	public function export() {
+        $this->rewardTable->prepare_items();
+		
+        $date = date('ymdHis');
+        $fileName = "{$date}_reward.csv";
+		
+		header('Content-Type: text/csv');
+		header("Content-Disposition: attachment; filename={$fileName}");
+        
+		$csvContent = implode($this->rewardTable->toCsvArray(), "\n");
+		echo mb_convert_encoding($csvContent, "SJIS", "UTF-8");
+		exit();
     }
 }
--- src/wp-content/themes/simplicity2/functions/support-rewards/support-reward-function.php
+++ src/wp-content/themes/simplicity2/functions/support-rewards/support-reward-function.php
@@ -2,10 +2,23 @@
 
 require('class/support-rewards-controller.php');
 
+const ADMIN_REWARD_PAGE="support-reward";
+
 add_action('admin_menu', 'init_support_reward_page', 11);
 
 function init_support_reward_page()
 {
     $supportReward = new SupportRewardsController();
-    add_menu_page('報酬確認', '報酬', 'administrator', 'support-reward', [$supportReward, 'index']); 
+	if ($_GET['content-type'] === 'csv' && $_GET['page'] === ADMIN_REWARD_PAGE) {
+		$supportReward->export();
+	}
+	
+    add_menu_page('報酬確認', '報酬', 'administrator', ADMIN_REWARD_PAGE, [$supportReward, 'index']); 
+}
+
+add_action('admin_enqueue_scripts', 'add_reward_admin_css');
+
+function add_reward_admin_css() {
+	$rewardAdminCssPath = '/wp-content/themes/simplicity2/functions/support-rewards/reward-admin.css';
+    wp_enqueue_style('custom', $rewardAdminCssPath);
 }
