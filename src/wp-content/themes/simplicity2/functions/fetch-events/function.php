7a5f3bd57b817eb40fabae2361ef0593255e6780
nuno929 <t-mark5@kce.biglobe.ne.jp>
2018-11-13 21:12:31 +0900

    fix : タグチェックの作成

--- src/wp-content/themes/simplicity2/functions/fetch-events/function.php
+++ src/wp-content/themes/simplicity2/functions/fetch-events/function.php
@@ -1,5 +1,12 @@
 <?php 
 
+// テスト環境、本番共通
+const PLEMIUM_EVENT_TERM_ID = 174;
+
+const CAFE_EVENT_TERM_ID = 250;
+
+const SEMINER_EVENT_TERM_ID = 337;
+
 if (!class_exists('TopPageEventsProvider')) {
   	require_once('top-page-events-provider.php');
 } 
@@ -11,10 +18,22 @@ function get_top_pege_events_provider() {
 	}
 
 	$top_page_events_provider = new TopPageEventsProvider();
-	add_actions('pre_get_posts', [&$top_page_events_provider, 'get_posts']);
-} 
-function get_top_page_events() {
-	$top_page_events_ids = 
-	
+	add_actions('posts_results', [&$top_page_events_provider, 'get_posts']);
 	add_filter('the_post', [&$top_page_events_ids, 'the_post']);
+	add_filter('get_event_type', [&$top_page_events_provider, 'get_event_type']);
+}
+
+function is_premium() {
+	$term_id = apply_filters('get_event_type', get_the_ID());
+	return $term_id === PLEMIUM_EVENT_TERM_ID;
+}
+
+function is_cafe() {
+	$term_id = apply_filters('get_event_type', get_the_ID());
+	return $term_id === CAFE_EVENT_TERM_ID;
+}
+
+function is_seminer() {
+	$term_id = apply_filters('get_event_type', get_the_ID());
+	return $term_id === SEMINER_EVENT_TERM_ID;
 }
--- src/wp-content/themes/simplicity2/functions/fetch-events/top-page-events-provider.php
+++ src/wp-content/themes/simplicity2/functions/fetch-events/top-page-events-provider.php
@@ -25,7 +25,7 @@ class TopPageEventsProvider
 
 		$post_metas = $wpdb->get_row($query->get_query(), ARRAY_A);
 		foreach ($post_metas as $post_meta) {
-			$is_premium = $post_meta['term_id'] === self::PLEMIUM_EVENT_TERM_ID;
+			$is_premium = $post_meta['term_id'] === PLEMIUM_EVENT_TERM_ID;
 			$in_two_week = $post_meta['event_date'] <= $this->two_week_later_time;
 			
 			if ($is_premium || $in_two_week) {
@@ -53,20 +53,29 @@ class TopPageEventsProvider
 		);
 	}
 
+	private function get_event_metas()
+	{
+		return array_merge(
+			$this->premium_event_metas,
+			$this->cafe_event_metas,
+			$this->seminer_event_metas
+		);
+	}
+
 	private function classify_post_metas($post_meta)
 	{
 		switch ($post_meta['term_id']) {
-			case self::PLEMIUM_EVENT_TERM_ID:
+			case PLEMIUM_EVENT_TERM_ID:
 				$max_show = self::MAX_SHOW_PLEMIUM_EVENT;
 				$array = &$this->premium_event_metas;
 				break;
 
-			case self::CAFE_EVENT_TERM_ID:
+			case CAFE_EVENT_TERM_ID:
 				$max_show = self::MAX_SHOW_CAFE_EVENT;
 				$array = &$this->cafe_event_metas;
 				break;
 
-			case self::SEMINER_EVENT_TERM_ID:
+			case SEMINER_EVENT_TERM_ID:
 				$max_show = self::MAX_SHOW_SEMINER_EVENT;
 				$array = &$this->seminer_event_metas;
 				break;
@@ -81,5 +90,36 @@ class TopPageEventsProvider
 
 		$post_id = $post_meta['post_id'];
 		$array[$post_id] = $post_meta;
+	}
+
+	public function posts_results($wp_query)
+	{
+		usort($wp_query->posts, [&$this, 'compare_events']);
+	}
+
+	private function compare_events($post, $nextPost)
+	{
+		$event_metas = $this->get_event_metas();
+		$post_meta = $event_metas[$post->ID];
+		$next_post_meta = $event_metas[$nextPost->ID];
+
+		if ($post_meta['term_id'] === $next_post_meta['term_id']) {
+			$post_date = $post_meta['_eventorganiser_schedule_start_start'];
+			$next_post_date = $next_post_meta['_eventorganiser_schedule_start_start'];
+
+			return $post_date < $next_post_date ? 1 : -1;
+		}
+
+		return $post_meta['term_id'] < $next_post_meta['term_id'] ? 1 : -1;
+	}
+
+	public function get_event_type($post_id)
+	{
+		$event_metas = $this->get_event_metas();
+		if (array_key_exists($post_id, $event_metas)) {
+			return $event_metas[$post_id]['term_id'];
+		}
+
+		return null;
 	}
 }
